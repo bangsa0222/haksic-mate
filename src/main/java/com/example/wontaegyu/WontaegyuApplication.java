0fa0d2b2bf36bfaa8be6203eaadce1ba54892083
jinwoo <chlwlsdn1215@naver.com>
2026-05-26 21:53:32 +0900

    왔다감

--- src/main/java/com/example/wontaegyu/WontaegyuApplication.java
+++ src/main/java/com/example/wontaegyu/WontaegyuApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WontaegyuApplication {
 
     public static void main(String[] args) {
+        System.out.println("왔다감");
         SpringApplication.run(WontaegyuApplication.class, args);
     }
 

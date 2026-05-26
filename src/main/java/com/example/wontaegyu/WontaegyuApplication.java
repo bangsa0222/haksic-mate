5e606add43fc4bd5f8878b16326c278ae490ad42
test <bangsa0222@gmail.com>
2026-05-26 20:06:44 +0900

    Initial commit

---
+++ src/main/java/com/example/wontaegyu/WontaegyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.wontaegyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WontaegyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WontaegyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wontaegyu/WontaegyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wontaegyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WontaegyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
